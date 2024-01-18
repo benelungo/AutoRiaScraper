82da3704da53808cee5668095be907b86105a169
benelungo <143945424+benelungo@users.noreply.github.com>
2024-01-19 00:19:46 +0200

    Scrapers were split on Main page and Car page scrapers

--- model/Scraper.py
+++ model/Scraper.py
@@ -2,22 +2,26 @@ import time
 
 import requests
 from bs4 import BeautifulSoup
-from model.Car import Car
-from model.Parser import CarPageParser, MainPageParser
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from threading import Thread, Semaphore
 
-proxy = open("proxies.txt").read().split("\n")
+from model.Car import Car
+from model.Parser import CarPageParser, MainPageParser
 
-num_of_threads = len(proxy) < 30 and len(proxy) or 10
+num_of_threads = 5
 
 
-class Scraper:
-    def __init__(self, url):
-        self.url = url
-        self.cars = []
+class MainPageScraper:
+    def __init__(self):
+        self.threads = []
         self.car_urls = []
         self._page_is_last = False
-        self._active_threads = 0
         self._semaphore = Semaphore(num_of_threads)
 
     @staticmethod
@@ -28,18 +32,8 @@ class Scraper:
             raise Exception("Request failed with status code: " + str(response.status_code))
         return BeautifulSoup(response.text, 'html.parser')
 
-    def _scrap_car_page(self, url: str, proxies=None) -> None:
-        # extends list of car objects
-        self._active_threads += 1
-        soup = self._make_request(url, proxies)
-        parser = CarPageParser(soup)
-        car = Car(url, *parser.get_car_info())
-        self.cars.append(car)
-        self._active_threads -= 1
-
     def _scrap_main_page(self, url, proxies=None) -> None:
         # extend list of car links
-        self._active_threads += 1
         soup = self._make_request(url, proxies)
         parser = MainPageParser(soup)
         car_items = parser.get_car_items()
@@ -48,36 +42,85 @@ class Scraper:
         self.car_urls.extend([item.get("href") for item in car_items])
         print("Found car urls: " + str(len(self.car_urls)))
         self._semaphore.release()
-        self._active_threads -= 1
 
-    def _fill_car_urls(self, start_page=0, end_page=0) -> None:
-        page_number = start_page
-        page_size = "100"
-
-        while not self._page_is_last and page_number <= end_page:
+    def _fill_car_urls(self, url, start_page=0, end_page=0, page_size='10', proxies=None) -> None:
+        self.threads = []
+        while not self._page_is_last and end_page and start_page <= end_page:
             self._semaphore.acquire()
-            url = self.url + '&page=' + str(page_number) + '&size=' + page_size
-            proxies = {'http': proxy[page_number % len(proxy)]}
-            Thread(target=self._scrap_main_page, args=(url, proxies)).start()
-            page_number += 1
+            search_url = url + '&page=' + str(start_page) + '&size=' + page_size
+            if proxies:
+                proxy = {'http': "http://" + proxies[start_page % len(proxies)]}
+            else:
+                proxy = None
+            thread = Thread(target=self._scrap_main_page, args=(search_url, proxy))
+            self.threads.append(thread)
+            thread.start()
+            start_page += 1
 
-    def _fill_cars(self) -> None:
-        for i in range(len(self.car_urls)):
-            self._semaphore.acquire()
-            proxies = {'http': proxy[i % len(proxy)]}
-            Thread(target=self._scrap_car_page, args=(self.car_urls[i], proxies)).start()
-
-    def scrap(self, start_page, end_page) -> None:
-        start = time.time()
-        self._fill_car_urls(start_page, end_page)
-        while self._active_threads != 0:
+    def scrap(self, url, start_page, end_page, page_size='10', proxies=None) -> None:
+        self._fill_car_urls(url, start_page, end_page-1, page_size, proxies)
+        while [thread.is_alive() for thread in self.threads].count(True) != 0:
             time.sleep(1)
         print("Total car urls found: " + str(len(self.car_urls)))
-        end = time.time()
-        print(f'Scraping time: {end - start} seconds')
 
-        print("Scrapping car info...")
-        # self._fill_cars()
-        print("Scraped cars: " + str(len(self.cars)))
-        while self._active_threads != 0:
-            time.sleep(1)
+
+class CarPageScraper:
+    def __init__(self, proxy=None):
+        self.proxy = proxy
+        self.driver = self._setupDriver()
+        self.first_run = True
+        self._semaphore = Semaphore(num_of_threads)
+
+    def _setupDriver(self):
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_argument("--disable-notifications")
+
+        prefs = {"profile.default_content_setting_values.notifications": 2,}
+        options.add_experimental_option("prefs", prefs)
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+
+        if self.proxy:
+            options.add_argument('--proxy-server=%s' % self.proxy)
+
+        return webdriver.Chrome(options=options)
+
+    def _click_number_button(self):
+        try:
+            WebDriverWait(self.driver, 300).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="phonesBlock"]/div/span/a'))).click()
+        except Exception as e:
+            print(e)
+
+    def _click_cookie_banner(self):
+        self.first_run = False
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="gdpr-notifier"]/div[1]/div[2]/label[1]'))).click()
+
+    def get_page_soup(self, url):
+        print("Scraping car url: " + url)
+        self.driver.get(url)
+        self.driver.implicitly_wait(30)
+        if self.first_run:
+            self._click_cookie_banner()
+        self._click_number_button()
+        self.driver.implicitly_wait(10)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        print("Done")
+        return soup
+
+    def scrap_car_page(self, url: str) -> Car:
+        soup = self.get_page_soup(url)
+        parser = CarPageParser(soup)
+        return Car(url, *parser.get_car_info())
+
+    def scrap(self, urls) -> list:
+        cars = []
+        for url in urls:
+            cars.append(self.scrap_car_page(url))
+        return cars
+
+    def __del__(self):
+        self.driver.quit()
