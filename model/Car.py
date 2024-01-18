5b025dd6d4e8e2026f1dfe115822c24ad32b5f5f
benelungo <143945424+benelungo@users.noreply.github.com>
2024-01-19 00:21:25 +0200

    Added some useful methods

--- model/Car.py
+++ model/Car.py
@@ -11,10 +11,9 @@
 # datetime_found (дата сохранения в базу);
 
 class Car:
-    def __init__(self, id_: int, url: str, title: str, price_usd: int,
+    def __init__(self, url: str, title: str, price_usd: int,
                  odometer: int, username: str, phone_number: int, image_url: str, images_count: int,
                  car_number: str, car_vin: str, datetime_found: str):
-        self.id = id_
         self.url = url
         self.title = title
         self.price_usd = price_usd
@@ -27,3 +26,21 @@ class Car:
         self.car_vin = car_vin
         self.datetime_found = datetime_found
 
+    def get_all(self):
+        return [self.url, self.title, self.price_usd, self.odometer, self.username, self.phone_number, self.image_url,
+                self.images_count, self.car_number, self.car_vin, self.datetime_found]
+
+    def print(self):
+        print("\nCar:")
+        print(self.url)
+        print(self.title)
+        print(self.price_usd)
+        print(self.odometer)
+        print(self.username)
+        print(self.phone_number)
+        print(self.image_url)
+        print(self.images_count)
+        print(self.car_number)
+        print(self.car_vin)
+        print(self.datetime_found)
+        print()
