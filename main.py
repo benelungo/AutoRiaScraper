0b84e46c6575f21725252f0ac0430daae9b2cee4
benelungo <143945424+benelungo@users.noreply.github.com>
2024-01-19 01:20:56 +0200

    Main file adjusted to start main function every day at 00:00. Main function scrapes AutoRia and saves data to DB, after scraping creates DB dump file

---
+++ main.py
@@ -0,0 +1,56 @@
+import time
+from threading import Thread, RLock
+import schedule
+from model.DB import DB
+from model.Scraper import MainPageScraper, CarPageScraper
+from model.db_config import db_name, user, password, host
+
+proxies = open("proxies.txt").read().split("\n")
+
+
+db = DB(dbname=db_name, user=user, password=password, host=host)
+db.drop_table()
+db.create_table()
+
+start = 0
+end = 4
+step = 2
+page_size = "100"
+cars = []
+num_of_threads = 4
+
+lock = RLock()
+
+
+def page_scrap_thread(urls):
+    sel = CarPageScraper()
+    for car in sel.scrap(urls):
+        lock.acquire()
+        db.insert(car)
+        lock.release()
+
+
+def main():
+    threads = []
+    for i in range(start, end, step):
+        scraper = MainPageScraper()
+        scraper.scrap("https://auto.ria.com/uk/search/?indexName=auto", i, i+step, page_size=page_size, proxies=proxies)
+        urls = scraper.car_urls
+
+        for j in range(num_of_threads):
+            print(F"Scraping from {len(urls) // num_of_threads * j} to {len(urls) // num_of_threads * (j + 1)}")
+            urls_chunk = urls[len(urls) // num_of_threads * j:len(urls) // num_of_threads * (j + 1)]
+            thread = Thread(target=page_scrap_thread, args=(urls_chunk,))
+            thread.start()
+            threads.append(thread)
+
+        while [thread.is_alive() for thread in threads].count(True) != 0:
+            time.sleep(1)
+    db.dump()
+
+
+schedule.every().days.at("00:00").do(main).run()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
