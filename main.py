bb2251782a2fb73967e255c2fa852a69c8978667
benelungo <143945424+benelungo@users.noreply.github.com>
2024-01-19 22:57:52 +0200

    Threads replaced by processes, works much faster

--- main.py
+++ main.py
@@ -1,63 +1,77 @@
 import time
-from threading import Thread, RLock
+from multiprocessing import Process, RLock
+from threading import Thread
+
 import schedule
 from model.DB import DB
 from model.Scraper import MainPageScraper, CarPageScraper
-from model.db_config import db_name, user, password, host
+from model.db_config import db_params
 
 proxies = open("proxies.txt").read().split("\n")
 
-
-db = DB(dbname=db_name, user=user, password=password, host=host)
-db.drop_table()
-db.create_table()
-
 start = 0
-end = 100000
-step = 5000
+end = 3000
+step = 5
 page_size = "100"
-cars = []
-num_of_threads = 4
+num_of_threads = 8
 
 lock = RLock()
 
 
-def page_scrap_thread(urls):
-    sel = CarPageScraper()
-    for car in sel.scrap(urls):
-        lock.acquire()
-        db.insert(car)
-        lock.release()
-    del sel
+class Main:
+    @staticmethod
+    def dump_and_clear_db():
+        db = DB(**db_params)
+        print(db.get_all())
+        db.dump()
+        db.drop_table()
+        db.create_table()
+        del db
+
+    @classmethod
+    def page_scrap_thread(cls, urls):
+        sel = CarPageScraper()
+        db = DB(**db_params)
+        with lock:
+            for car in sel.scrap(urls):
+                    db.insert(car)
+        del sel
+        del db
+
+    @classmethod
+    def main(cls):
+        processes = []
+        cls.dump_and_clear_db()
+
+        for i in range(start, end, step):
+            scraper = MainPageScraper()
+            scraper.scrap("https://auto.ria.com/uk/search/?indexName=auto", i, i+step, page_size=page_size, proxies=proxies)
+            urls = scraper.car_urls
+            if not urls:
+                print("Last page reached!")
+                break
+
+            for j in range(num_of_threads):
+                print(F"Scraping from {len(urls) // num_of_threads * j} to {len(urls) // num_of_threads * (j + 1)}")
+                urls_chunk = urls[len(urls) // num_of_threads * j:len(urls) // num_of_threads * (j + 1)]
+                process = Process(target=cls.page_scrap_thread, args=(urls_chunk,))
+                process.start()
+                processes.append(process)
+
+            for process in processes:
+                process.join()
+
+            print("Sleeping for 15 seconds...")
+            time.sleep(15)
+        cls.insert_thread_run = False
+        # insert_thread.kill()
 
 
-def main():
-    threads = []
-    for i in range(start, end, step):
-        scraper = MainPageScraper()
-        scraper.scrap("https://auto.ria.com/uk/search/?indexName=auto", i, i+step, page_size=page_size, proxies=proxies)
-        urls = scraper.car_urls
-        if not urls:
-            print("Last page reached!")
-            break
+# schedule.every().days.at("00:00").do(main).run()
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
-        for j in range(num_of_threads):
-            print(F"Scraping from {len(urls) // num_of_threads * j} to {len(urls) // num_of_threads * (j + 1)}")
-            urls_chunk = urls[len(urls) // num_of_threads * j:len(urls) // num_of_threads * (j + 1)]
-            thread = Thread(target=page_scrap_thread, args=(urls_chunk,))
-            thread.start()
-            threads.append(thread)
-
-        while [thread.is_alive() for thread in threads].count(True) != 0:
-            time.sleep(1)
-
-        print("Sleeping for 30 seconds...")
-        time.sleep(30)
-    db.dump()
-
-
-schedule.every().days.at("00:00").do(main).run()
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == '__main__':
+    Main().main()
